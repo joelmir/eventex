1d364d64322e59655f56edc89f1ceb3642c1b217
unknown <Joelmir@.(none)>
2011-08-26 21:42:08 -0300

    adicionando app subscription

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render_to_response
+
 def homepage(request):
     from django.conf import settings
     context = {'STATIC_URL': settings.STATIC_URL}
--- urls.py
+++ urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from core.views import homepage
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', homepage),
+    url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', homepage),
     # url(r'^eventex/', include('eventex.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += staticfiles_urlpatterns()
